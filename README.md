f07cbfbf6471f9bfd1be529ccb5e37a6b1fd1122
Cat-JiYoung <kimyamae82@gmail.com>
2023-05-14 19:50:30 +0900

    [chore] init

--- README.md
+++
@@ -1,2 +0,0 @@
-# Lotte-Cinema-AOS
-중요한건 꺾이지 않는 안드 💪💚
---
+++ app/src/main/java/com/sopt/lottecinemaaos/presentation/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sopt.lottecinemaaos.presentation.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sopt.lottecinemaaos.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
