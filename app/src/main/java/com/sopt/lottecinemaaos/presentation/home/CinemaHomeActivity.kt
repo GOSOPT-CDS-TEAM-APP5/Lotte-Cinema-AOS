6b0dc791bd6a8cb41b529deeaa062fd340a8b9d0
ss99x2002 <ss99x200211@gmail.com>
2023-05-17 20:43:46 +0900

    [feat] home view 상단 ViewPager 구현

--- app/src/main/java/com/sopt/lottecinemaaos/presentation/home/CinemaHomeActivity.kt
+++ app/src/main/java/com/sopt/lottecinemaaos/presentation/home/CinemaHomeActivity.kt
@@ -3,10 +3,28 @@ package com.sopt.lottecinemaaos.presentation.home
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sopt.lottecinemaaos.R
+import com.sopt.lottecinemaaos.databinding.ActivityCinemaHomeBinding
+import com.sopt.lottecinemaaos.util.base.BindingActivity
 
-class CinemaHomeActivity : AppCompatActivity() {
+class CinemaHomeActivity :
+    BindingActivity<ActivityCinemaHomeBinding>(R.layout.activity_cinema_home) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cinema_home)
+        setViewHolder()
+    }
+
+    private fun setViewHolder() {
+        with(binding) {
+            layoutTopViewpager.adapter = CinemaHomeVPAdapter().apply {
+                setItemList(
+                    listOf(
+                        R.drawable.img_home_viewpager2,
+                        R.drawable.img_home_viewpager3,
+                        R.drawable.img_home_viewpager1
+                    )
+                )
+            }
+            wormDotsIndicator.attachTo(layoutTopViewpager)
+        }
     }
 }
