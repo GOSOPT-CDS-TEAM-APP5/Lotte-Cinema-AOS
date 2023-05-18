b8ff9bd81c01b92e9f35be9b4936cb3dcf16af37
Cat-JiYoung <kimyamae82@gmail.com>
2023-05-18 16:46:53 +0900

    [feat] 지역선택 recyclerView 구현

---
+++ app/src/main/java/com/sopt/lottecinemaaos/data/entity/Region.kt
@@ -0,0 +1,8 @@
+package com.sopt.lottecinemaaos.data.entity
+
+data class Region(
+    val id: Int,
+    val regionName: String,
+    val regionCount: Int
+
+)
--- app/src/main/java/com/sopt/lottecinemaaos/presentation/selection/CinemaSelectionViewModel.kt
+++ app/src/main/java/com/sopt/lottecinemaaos/presentation/selection/CinemaSelectionViewModel.kt
@@ -1,5 +1,21 @@
 package com.sopt.lottecinemaaos.presentation.selection
 
 import androidx.lifecycle.ViewModel
+import com.sopt.lottecinemaaos.data.entity.Region
 
-class CinemaSelectionViewModel : ViewModel()
+class CinemaSelectionViewModel : ViewModel() {
+    val regionList: List<Region> =
+        listOf(
+            Region(1, "MY 영화관", 0),
+            Region(2, "가까운 영화관", 1),
+            Region(3, "서울", 2),
+            Region(4, "경기", 3),
+            Region(5, "인천", 4),
+            Region(6, "충청", 5),
+            Region(7, "전라", 6),
+            Region(8, "경북", 7),
+            Region(9, "대구", 8),
+            Region(10, "경남", 9)
+
+        )
+}
