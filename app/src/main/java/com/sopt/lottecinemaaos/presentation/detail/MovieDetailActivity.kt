119a345c0326275c162072a00fcb0d6c88d34684
emjayMJkim <minjeong030420@gmail.com>
2023-05-23 03:40:11 +0900

    [feat] 트레일러&포스터 recyclerView adapter 연결

--- app/src/main/java/com/sopt/lottecinemaaos/presentation/detail/MovieDetailActivity.kt
+++ app/src/main/java/com/sopt/lottecinemaaos/presentation/detail/MovieDetailActivity.kt
@@ -1,12 +1,43 @@
 package com.sopt.lottecinemaaos.presentation.detail
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.viewModels
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sopt.lottecinemaaos.R
+import com.sopt.lottecinemaaos.databinding.ActivityMovieDetailBinding
+import com.sopt.lottecinemaaos.util.base.BindingActivity
 
-class MovieDetailActivity : AppCompatActivity() {
+class MovieDetailActivity : BindingActivity<ActivityMovieDetailBinding>(R.layout.activity_movie_detail) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_detail)
+
+
+        setTrailer()
+        setPoster()
+
+        //setContentView(R.layout.activity_movie_detail)
+    }
+
+    private  val viewModel by viewModels<MovieDetailViewModel>()
+
+    private fun setTrailer() {
+        val trailerAdapter = MovieDetailTrailerAdapter(this)   //
+        trailerAdapter.setList(viewModel.trailerList)                      //
+
+        with (binding) {
+            rcvDetailTrailer.adapter = trailerAdapter
+            rcvDetailTrailer.layoutManager = LinearLayoutManager(this@MovieDetailActivity, LinearLayoutManager.HORIZONTAL, false)
+        }
+    }
+
+    private fun setPoster() {
+        val posterAdapter = MovieDetailPosterAdapter(this)
+        posterAdapter.setList(viewModel.posterList)
+
+        with (binding) {
+            rcvDetailPoster.adapter = posterAdapter
+            rcvDetailPoster.layoutManager = LinearLayoutManager(this@MovieDetailActivity, LinearLayoutManager.HORIZONTAL, false)
+        }
     }
 }
