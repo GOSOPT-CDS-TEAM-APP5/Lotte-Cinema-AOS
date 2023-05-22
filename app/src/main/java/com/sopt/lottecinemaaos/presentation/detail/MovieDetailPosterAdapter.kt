5d6a78ae54643f0bc058d62490e039f9fac16781
emjayMJkim <minjeong030420@gmail.com>
2023-05-23 03:40:11 +0900

    [feat] 트레일러&포스터 recyclerView adapter 연결

---
+++ app/src/main/java/com/sopt/lottecinemaaos/presentation/detail/MovieDetailPosterAdapter.kt
@@ -0,0 +1,40 @@
+package com.sopt.lottecinemaaos.presentation.detail
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.sopt.lottecinemaaos.databinding.ItemDetailPosterBinding
+import com.sopt.lottecinemaaos.domain.model.Poster
+
+class MovieDetailPosterAdapter(context: Context) :RecyclerView.Adapter<MovieDetailPosterAdapter.DetailPosterViewHolder>() {
+
+    private val inflater by lazy { LayoutInflater.from(context) }
+
+    private var itemList: List<Poster> = emptyList()
+
+    class DetailPosterViewHolder(private val binding: ItemDetailPosterBinding) :
+            RecyclerView.ViewHolder(binding.root) {
+                fun onBind(item: Poster) {
+                    binding.imgItemPoster.setImageDrawable(binding.root.context.getDrawable(item.src))
+                }
+            }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailPosterViewHolder {
+        val binding: ItemDetailPosterBinding = ItemDetailPosterBinding.inflate(inflater, parent, false)
+        return DetailPosterViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: DetailPosterViewHolder, position: Int) {
+        holder.onBind(itemList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    fun setList(posterList: List<Poster>) {
+        this.itemList = posterList.toList()
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/sopt/lottecinemaaos/presentation/detail/MovieDetailViewModel.kt
+++ app/src/main/java/com/sopt/lottecinemaaos/presentation/detail/MovieDetailViewModel.kt
@@ -1,5 +1,52 @@
 package com.sopt.lottecinemaaos.presentation.detail
 
 import androidx.lifecycle.ViewModel
+import com.sopt.lottecinemaaos.R
+import com.sopt.lottecinemaaos.domain.model.Poster
+import com.sopt.lottecinemaaos.domain.model.Trailer
 
-class MovieDetailViewModel : ViewModel()
+class MovieDetailViewModel : ViewModel() {
+    val trailerList = listOf<Trailer>(
+        Trailer(
+            src = R.drawable.img_trailer1,
+            title = "티저 예고편"
+        ),
+        Trailer(
+            src = R.drawable.img_trailer2,
+            title = "메인 예고편"
+        ),
+        Trailer(
+            src = R.drawable.img_trailer3,
+            title = "'돌아온 가디언즈' 30초 예고편"
+        ),
+        Trailer(
+            src = R.drawable.img_trailer4,
+            title = "Team '가디언즈'가 맞설 마지막 미션!"
+        ),
+        Trailer(
+            src = R.drawable.img_trailer5,
+            title = "전 세계 최초 개봉! 전 포맷 예매 오픈!"
+        )
+    )
+
+    val posterList = listOf<Poster>(
+        Poster(
+            src = R.drawable.img_poster1
+        ),
+        Poster(
+            src = R.drawable.img_stillcut1
+        ),
+        Poster(
+            src = R.drawable.img_stillcut2
+        ),
+        Poster(
+            src = R.drawable.img_stillcut3
+        ),
+        Poster(
+            src = R.drawable.img_stillcut4
+        ),
+        Poster(
+            src = R.drawable.img_stillcut5
+        ),
+    )
+}
